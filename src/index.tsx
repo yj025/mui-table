2107b65515d5d99354b57a2be6642d70aa677276
yinjun <jun.yin@thoughtworks.com>
2024-01-29 16:05:22 +0800

    init 1.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,142 +1,49 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {useRef, useState} from 'react';
+import Button from "@mui/material/Button";
 
+const Stopwatch = ()=>{
+    const [startTime,setStartTime]=useState(null);
+    const [now,setNow] = useState(null)
+    const intervalRef=useRef(null)
 
+    function handleStart() {
+        setStartTime(Date.now());
+        setNow(Date.now());
 
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
+    }
 
+    function handleStop() {
+        clearInterval(intervalRef.current);
+    }
+
+    let secondsPassed = 0;
+    if (startTime != null && now != null) {
+        secondsPassed = (now - startTime) / 1000;
+    }
+
+    return(
+        <>
+            <h1>Time Stop Watch</h1>
+            <Button onClick={handleStart}>Start</Button>
+            <Button onClick={handleStop}>Stop</Button>
+            <p>{secondsPassed}</p>
+        </>
+    )
+}
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-        <BasicTable/>
-    </StyledEngineProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <Stopwatch />
+        </StyledEngineProvider>
+    </React.StrictMode>
 );
 
 
-
-
-
-
-
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-) {
-    return { name, calories, fat, carbs, protein };
-}
-
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function sleep(duration: number) {
-    return new Promise<void>((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, duration);
-    });
-}
- function BasicTable() {
-     const tableStyle = {
-         height: "500px",
-         overflow: "auto"
-     }
-    const tableContainerRef = useRef(null); // 创建一个ref来引用表格容器
-     const [loading,setLoading]= useState(false)
-     const [loadedRows, setLoadedRows] = React.useState<any>([]);
-     const mounted = React.useRef(true);
-
-    const handleScroll = () => {
-        const tableContainer = tableContainerRef.current;
-        if (tableContainer) {
-            const scrollTop = tableContainer.scrollTop; // 当前滚动位置
-            const scrollHeight = tableContainer.scrollHeight; // 整个内容的高度
-            const clientHeight = tableContainer.clientHeight; // 可见窗口的高度
-            if (scrollTop + clientHeight >= scrollHeight-2) {
-                console.log(scrollTop,clientHeight,scrollHeight,'Table scrolled to the bottom.');
-                //set infiniteLoading true
-                //send request (sales web)=>update table data
-                //set infiniteLoading false (useEffect)
-            }
-        }
-    };
-    return (
-
-        <div style={tableStyle} ref={tableContainerRef} onScroll={handleScroll}>
-            <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table" >
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody >
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        </div>
-    );
-}
